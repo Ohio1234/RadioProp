7ce44329604b668c3e7b151a90d698dd917742fe
tobiasffa <70888884+tobiasffa@users.noreply.github.com>
2022-06-14 17:11:31 +0200

    Add files via upload

---
+++ RadioProp/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+games {'gta5'}
+
+author 'Radirop - Lavet af tubidubs#2495'
+description 'Radio prop der viser når du trykker LALT - Lavet af tubidubs#2495'
+version '2.0'
+
+
+client_scripts {
+	'radio.lua'
+}
