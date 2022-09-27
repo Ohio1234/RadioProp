b2a8b0aee39582490f95d3431eb53a10e9c968ed
Ohio <70888884+Ohio1234@users.noreply.github.com>
2022-09-27 14:34:52 +0200

    Update fxmanifest.lua

--- RadioProp/fxmanifest.lua
+++ RadioProp/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'adamant'
 games {'gta5'}
 
 author 'Radirop - ikke lavet af mig'
-description 'Radio prop der viser når du trykker LALT - ikke lavet af mig'
+description 'Radio prop der viser når du trykker LALT - lavet af mig'
 version '2.0'
 
 
